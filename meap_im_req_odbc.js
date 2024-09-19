3846f1d6ea6de26236189416b53c376dfbb1a965
kukul-hapi <92004218+kukul-hapi@users.noreply.github.com>
2024-09-19 13:52:09 +0800

    Create meap_im_req_odbc.js

---
+++ meap_im_req_odbc.js
@@ -0,0 +1,106 @@
+
+var odbc = require('./meap_odbc');
+var url = require('url');
+var meap_tmpl = require("./meap_im_tmpl");
+var pool = require("./meap_odbcpool");
+var async = Require("async");
+
+function run(option, callback, robot) {
+    LOG1("[meap_im_req_odbc][run]:ODBC option ", option);
+    var db = new odbc.Database();
+    db.open(option.CN, function (error) {
+        var self = db;
+        if (error) {
+            _ERROR("[meap_im_req_odbc][run][ERROR]:CONNECT FAIL,", error);
+            callback(-1, {
+                'status': '15100',
+                'message': error
+            });
+            return;
+        }
+        self.query(option.sql, function (error, rows, more) {
+            if (error) {
+                _ERROR("[meap_im_req_odbc][run][ERROR]:QUERY RESULT ", error);
+                callback(-1, {
+                    'status': '15101',
+                    'message': error
+                });
+            } else {
+                LOG3("[meap_im_req_odbc][run]:QUERY RESULT ", rows, more);
+                callback(0, rows, more);
+            }
+            db.close(function () {
+            });
+        });
+    });
+}
+
+function transaction(option, precb, itemcb, rescb, robot) {
+    //LOG4("[ODBC] ODBC option ", option);
+    var db = new odbc.Database();
+    db.open(option.CN, function (error) {
+        var self = db;
+        if (error) {
+            rescb(-1, {
+                'status': '15100',
+                'message': error
+            });
+            return;
+        }
+        var context = {};
+        self.beginTransaction(function (err) {
+            if (err) {
+                rescb(-1, {
+                    'status': '15101',
+                    'message': err
+                });
+                return db.close();
+            }
+            async.mapSeries(option.transaction, function (item, cb) {
+                var sql = precb ? precb(context, item) : item;
+                self.query(sql, function (error, rows, more) {
+                    if (error) {
+                        rescb(-1, {
+                            'status': '15101',
+                            'message': err
+                        });
+                        self.rollbackTransaction(function (err) {
+                            return db.close();
+                        });
+                    } else {
+                        var data = itemcb ? itemcb(item, rows, context) : {err: 0, res: rows};
+                        cb(data.err, data.res);
+                    }
+                });
+            }, function (err, res) {
+                if (!err) {
+                    self.commitTransaction(function (err) {
+                        rescb(err, res);
+                        return db.close();
+                    });
+                } else {
+                    rescb(-1, {
+                        'status': '15101',
+                        'message': err
+                    });
+                    self.rollbackTransaction(function (err) {
+                        return db.close();
+                    });
+                }
+            });
+        });
+    });
+}
+
+function buildstatement(sql, params, options, cb) {
+    options.sql = meap_tmpl.Runner(sql, params, cb);
+}
+
+function buildPool(option) {
+    return pool.odbcPool(option);
+}
+
+exports.BuildStatement = buildstatement;
+exports.Runner = run;
+exports.BuildPool = buildPool;
+exports.Transaction = transaction;
